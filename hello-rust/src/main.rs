b812ced1d8b97b40702a3482e1f8df8c558e24b1
mrtstr <martin0strauch@gmail.com>
2025-09-13 09:48:52 +0200

    added functions for saving parquets

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,15 +1,19 @@
 use clap::{Parser, ValueEnum};
 use anyhow::{Result, bail};
+use polars::prelude::*;
+use std::fs::File;
+use std::path::Path;
 
 /// A minimal calculator CLI
 #[derive(Parser, Debug)]
 #[command(name = "compute", version, about = "Example CLI")]
 struct Cli {
     /// First operand
-    x: i64,
+    path: String,
 
-    /// Second operand
-    y: i64,
+    /// Partition columns (use multiple values)
+    #[arg(long, num_args = 1..)]
+    part_columns: Vec<String>,
 
     /// Operation to apply
     #[arg(long, value_enum, default_value_t = Op::Add)]
@@ -24,21 +28,32 @@ enum Op {
     Div,
 }
 
+fn create_df() -> PolarsResult<DataFrame> {
+    let df = df![
+        "name"  => &["Alice", "Bob", "Charlie"],
+        "age"   => &[25i32, 30, 40],
+        "score" => &[88.5f64, 95.0, 79.2],
+        "city" => &["bs", "bs", "gi"],
+    ]?;
+    Ok(df)
+}
+
+fn write_parquet(df: &mut DataFrame, path: impl AsRef<Path>) -> Result<()> {
+    let file = File::create(path)?;
+    ParquetWriter::new(file)
+        .with_compression(ParquetCompression::Snappy)
+        .finish(df)?;  // <-- mutable
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let res = match args.op {
-        Op::Add => args.x + args.y,
-        Op::Sub => args.x - args.y,
-        Op::Mul => args.x * args.y,
-        Op::Div => {
-            if args.y == 0 {
-                bail!("division by zero");
-            }
-            args.x / args.y
-        }
-    };
+    let mut df = create_df()?;
+    write_parquet(&mut df, &args.path)?;
 
-    println!("{res}");
+
+    println!("Sucessfully saved the following df und {} \n{df}", args.path);
+    // println!("{}, {:?}", args.path, args.part_columns);
     Ok(())
 }
