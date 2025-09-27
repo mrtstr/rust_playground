1dd5ab174028170af1a785b42d538a727a32b5d6
mrtstr <martin0strauch@gmail.com>
2025-09-27 20:00:09 +0200

    add additional params

--- wrapped_example_py/wrapped_example/__init__.py
+++ wrapped_example_py/wrapped_example/__init__.py
@@ -4,6 +4,6 @@ from wrapped_example_core import df_sum_scores as _df_sum_scores
 
 __all__ = ["df_sum_scores"]
 
-def df_sum_scores(df: pl.DataFrame) -> pl.DataFrame:
+def df_sum_scores(df: pl.DataFrame, col_name: str = "score", out_name=None) -> pl.DataFrame:
     """Sum the 'score' column and return a one-row DataFrame with 'score_sum'."""
-    return _df_sum_scores(df)
+    return _df_sum_scores(df, col_name, out_name)
--- wrapped_example_rs/src/lib.rs
+++ wrapped_example_rs/src/lib.rs
@@ -1,42 +1,28 @@
 use pyo3::prelude::*;
 use pyo3_polars::PyDataFrame;
 use polars::prelude::*;
-use eyre::{Result, eyre}; // ergonomic error handling
+use eyre::Result;
 
 #[pyfunction]
-#[pyo3(signature = (pydf))]
-fn df_sum_scores(pydf: PyDataFrame) -> PyResult<PyDataFrame> {
+#[pyo3(signature = (pydf, col_name="score", out_name=None))]
+fn df_sum_scores(pydf: PyDataFrame, col_name: &str, out_name: Option<&str>) -> PyResult<PyDataFrame> {
     // Inner block returns eyre::Result<_>; `?` works everywhere.
     let out = (|| -> Result<DataFrame> {
         let col = pydf.0
-            .column("score")?
+            .column(col_name)?
             // .map_err(|e| eyre!("missing 'score' column: {e}"))? // add manually context
             .cast(&DataType::Float64)?;
 
         let sum: f64 = col.f64()?.sum().unwrap_or(0.0);
-        let out = df!("score_sum" => [sum])?;
+        let out_name_owned = out_name.map(str::to_string).unwrap_or_else(|| format!("{col_name}_sum"));
+        let out = df!(out_name_owned => [sum])?;
         Ok(out)
     })()?;
+    // add rust stack trace with the following
+    // })().map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(format!("{e:#}\n\n[Rust backtrace]\n{}", std::backtrace::Backtrace::capture())))?; 
     Ok(PyDataFrame(out))
 }
 
-// #[pyfunction]
-// #[pyo3(signature = (pydf))]
-// fn df_sum_scores(pydf: PyDataFrame) -> PyResult<PyDataFrame> {
-//     let df: DataFrame = pydf.0;
-//     let sum = df
-//         .column("score")
-//         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?
-//         .cast(&DataType::Float64)
-//         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?
-//         .f64()
-//         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?
-//         .sum().unwrap();
-
-//     let out = DataFrame::new(vec![Column::new("a_sum".into(), &[sum])]).map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
-
-//     Ok(PyDataFrame(out))
-// }
 
 #[pymodule]
 fn wrapped_example_core(_py: Python<'_>, m: &Bound<PyModule>) -> PyResult<()> {
