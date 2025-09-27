5af9c05a99d04ef63562b580767f92148fa6094f
mrtstr <martin0strauch@gmail.com>
2025-09-27 19:20:42 +0200

    make eager work

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ maturin develop -m wrapped_example_rs/Cargo.toml --release
 
 
 ```bash
+pip uninstall wrapped_example_core
+
 maturin build -m wrapped_example_rs/Cargo.toml \
       --release --strip --out dist \
       --compatibility musllinux_1_2
--- wrapped_example_rs/src/lib.rs
+++ wrapped_example_rs/src/lib.rs
@@ -1,24 +1,42 @@
 use pyo3::prelude::*;
 use pyo3_polars::PyDataFrame;
-
 use polars::prelude::*;
-use polars::prelude::{col, IntoLazy};
+use eyre::{Result, eyre}; // ergonomic error handling
 
-
-/// Sum the "score" column and return a one-row DataFrame with "score_sum".
 #[pyfunction]
 #[pyo3(signature = (pydf))]
 fn df_sum_scores(pydf: PyDataFrame) -> PyResult<PyDataFrame> {
-    let df: DataFrame = pydf.0; // take inner frame (avoid Into confusion)
+    // Inner block returns eyre::Result<_>; `?` works everywhere.
+    let out = (|| -> Result<DataFrame> {
+        let col = pydf.0
+            .column("score")?
+            // .map_err(|e| eyre!("missing 'score' column: {e}"))? // add manually context
+            .cast(&DataType::Float64)?;
 
-    let out = df
-        .lazy()
-        .select([col("score").sum().alias("score_sum")])
-        .collect()
-        .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
+        let sum: f64 = col.f64()?.sum().unwrap_or(0.0);
+        let out = df!("score_sum" => [sum])?;
+        Ok(out)
+    })()?;
     Ok(PyDataFrame(out))
 }
+
+// #[pyfunction]
+// #[pyo3(signature = (pydf))]
+// fn df_sum_scores(pydf: PyDataFrame) -> PyResult<PyDataFrame> {
+//     let df: DataFrame = pydf.0;
+//     let sum = df
+//         .column("score")
+//         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?
+//         .cast(&DataType::Float64)
+//         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?
+//         .f64()
+//         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?
+//         .sum().unwrap();
+
+//     let out = DataFrame::new(vec![Column::new("a_sum".into(), &[sum])]).map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
+
+//     Ok(PyDataFrame(out))
+// }
 
 #[pymodule]
 fn wrapped_example_core(_py: Python<'_>, m: &Bound<PyModule>) -> PyResult<()> {
